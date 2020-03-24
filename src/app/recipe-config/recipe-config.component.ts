16c39864f5342af7938e670e57c27f8230dfefa3
WilliamTempleton <william.templeton@centrale-marseille.fr>
2020-03-24 11:51:07 +0100

    Created components: main page, recipe list, recipe detail and recipe config

---
+++ src/app/recipe-config/recipe-config.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-config',
+  templateUrl: './recipe-config.component.html',
+  styleUrls: ['./recipe-config.component.css']
+})
+export class RecipeConfigComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
